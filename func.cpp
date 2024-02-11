efb46347696776f0f85a89116521d02742c65526
Pavel <pavel.img@gmail.com>
2024-02-11 03:13:23 +0200

    Laba 2 clean description png

--- func.cpp
+++ func.cpp
@@ -2,25 +2,4 @@
 #include <fstream>
 #include <string>
 
-int N::check_file() { //
-    std::string ignore_string;
-    std::ifstream myfile;
-    myfile.open(N::file_name);
-    int start = 0;
-    if (myfile.is_open()) {
-        while (std::getline(myfile, ignore_string)) {
-            start++;
-        }
-        return start;
-    }
-    return -1;
-}
-
-int N::create_file() {
-
-}
-
-
-    void N::do_something() {
-    std::cout << "Doing something!" << std::endl;
-}
+class square()
--- func.h
+++ func.h
@@ -1,8 +1,6 @@
 #include <iostream>
 namespace N
 {
-    const std::string file_name = "time_list.txt";
-    void do_something();
-    int check_file();
+
 
 }
